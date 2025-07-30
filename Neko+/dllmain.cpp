df59368a8f3b7ca06d3867a11edd65e0191239a5
xuntf <xuntf@CYBERPOWER>
2025-07-29 20:23:03 -0400

    Now it actually injects, all it does is change the window title right now

--- Neko+/dllmain.cpp
+++ Neko+/dllmain.cpp
@@ -8,10 +8,32 @@
 #include "winrt/Windows.UI.ViewManagement.h"
 #include "winrt/Windows.Foundation.h"
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID reserved) {
-    switch (reason) {
+void setWindowTitle(const std::wstring& title) {
+    winrt::Windows::ApplicationModel::Core::CoreApplication::MainView().CoreWindow().DispatcherQueue().TryEnqueue([title]() {
+        winrt::Windows::UI::ViewManagement::ApplicationView::GetForCurrentView().Title(title);
+        });
+}
+
+DWORD WINAPI initClient(LPVOID lpParameter) {
+    setWindowTitle(L"");
+
+    Client::getInstance().initialize();
+
+    setWindowTitle(L"Neko+");
+
+    while (Client::getInstance().isInitialized()) {
+        Sleep(50);
+    }
+
+    Sleep(25);
+    FreeLibraryAndExitThread((HMODULE)lpParameter, 1);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
-        Client::getInstance().initialize();
+        DisableThreadLibraryCalls(hModule); 
+        CreateThread(nullptr, 0, initClient, hModule, 0, nullptr);
         break;
 
     case DLL_PROCESS_DETACH:
